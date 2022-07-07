e5d1e649b2a2b293e6a126ac660c4fe61c37379a
yogita mahale <Dell@DESKTOP-EOMD4C5>
2022-07-07 19:53:58 +0100

    Add project files.

---
+++ BookProject/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BookProject.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books {get;set;}
+    }
+}
